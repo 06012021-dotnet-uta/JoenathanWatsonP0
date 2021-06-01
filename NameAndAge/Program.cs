9ffeba5f0f132f19a927d87e7206548f91e1ef99
Your Name <you@example.com>
2021-06-01 17:53:30 -0400

    my first commit

---
+++ NameAndAge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NameAndAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("what is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("what is your age ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"mayname is {name} and my age is {age}");
+        }
+    }
+}
